c4b9b986a564d17412c12ca856ce8489ff788f17
Alberto Jaramillo <djaramillo@unamad.edu.pe>
2024-07-11 21:22:44 -0500

    iniciios en python

---
+++ dia1/1_variables.py
@@ -0,0 +1,26 @@
+nombre = 'Denys'
+
+
+edad = 32
+
+soltero = True
+
+sueldo = 870.35
+
+nombre = -1
+# en python los tipo de variables pueden cambiar sin ninguna restricción
+
+texto1= "Hola yo me llamo"
+texto2 = "  Demys"
+
+resultado = texto1 + texto2
+print(resultado)
+
+alumno1, alumno2, alumno3 = 'Ignacio', 'Segundo', 'Abel'
+print(alumno1.upper())
+
+
+# split > separa el texto en base al caractere de busqueda
+
+resultado = texto1.split(' ')
+print(resultado)
