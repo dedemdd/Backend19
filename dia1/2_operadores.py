c1e5f3e44df717324a78a9baf2e69383bcbda510
Alberto Jaramillo <djaramillo@unamad.edu.pe>
2024-07-12 21:29:08 -0500

    Incluye dia

---
+++ dia1/2_operadores.py
@@ -0,0 +1,26 @@
+numero1 = 10
+numero2 = 20
+
+#Aritmenticos
+print(numero1 + numero2) #Suma
+print(numero1 - numero2) #Resta
+print(numero1 * numero2) #multiplicacion
+print(numero1 / numero2) #division
+print(numero1 // numero2) #division entera
+print(numero1 % numero2) #modulo
+print(numero1 ** numero2) #potencia
+
+#Comparacion
+
+print(numero1 == numero2) #igualdad
+print(numero1 != numero2) #diferencia
+print(numero1 > numero2) #mayor que
+print(numero1 < numero2) #menor que
+print(numero1 >= numero2) #mayor o igual que
+print(numero1 <= numero2) #menor o igual que
+
+#Logicos
+
+print(numero1 > numero2 and numero1 != numero2) #AND
+print(numero1 > numero2 or numero1 != numero2) #OR
+print(not numero1 > numero2) #NOT invierte el resultado de la comparación
---
+++ dia1/3_strings.py
@@ -0,0 +1,24 @@
+texto = "Hola a todos como están"
+
+texto2 = 'Hola a todos como están'
+
+#Bloque de texto
+texto_largo = """y Juanita exclamó: Ojalá mañana sea viernes
+A lo que Pedrito dijo: Pero si mañana ya es viernes!!
+Y Ramón adicionó: Hay festival de carnes y cerveza"""
+print(texto_largo)
+
+print(texto[1])
+print(texto[-1])
+
+#extraer una subcadena del texto
+print(texto[1:6])
+
+#desde la posicion 0 ashta la posicion 10
+print(texto[:10])
+
+#desde la posicion 20 ashta la posicion final
+print(texto[20:])
+
+print(texto[-2:])
+print(texto[4:2])
